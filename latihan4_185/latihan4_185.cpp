f7861275c1390b1ea0f35b0280a5e5299f6910bf
nur hikmah <nur.hikmah190504@gmail.com>
2023-03-20 18:49:00 +0700

    int

--- latihan4_185/latihan4_185.cpp
+++ latihan4_185/latihan4_185.cpp
@@ -11,3 +11,46 @@ float luasSegitiga(float a, float t) {
 	return 0.5 * a * t;
 }
 
+int main()
+{
+	float panjang, lebar, jejari, alas, tinggi;
+	int pilihan;
+
+	do {
+		cout << "\n\n================" << endl;
+		cout << "=======MENU=========" << endl;
+		cout << "====================" << endl;
+		cout << "1. Luas Persegi" << endl;
+		cout << "2. Luas Lingkaran" << endl;
+		cout << "3. Luas Segitiga" << endl;
+		cout << "4. Exit" << endl;
+		cout << "pilihan (1/2/3/4): ";
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+		case 1:
+			cout << "Masukan Panjang = ";
+			cin >> panjang;
+			cout << "masukan Lebar = ";
+			cin >> lebar;
+			cout << " Luas Persegi Panjang = " << LuasPersegi(panjang, lebar);
+			break;
+		case 2:
+			cout << "Masukan jari - jari = ";
+			cin >> jejari;
+			cout << "Luas Lingkaran = " << LuasLingkaran(jejari);
+			break;
+		case 3:
+			cout << "Masukan Alas = ";
+			cin >> alas;
+			cout << "Masukan Tinggi = ";
+			cin >> tinggi;
+		case 4:
+			break;
+		default:
+			cout << "PILIHAN SALAH" << endl;
+			break;
+		}
+	} while (pilihan != 4);
+}
