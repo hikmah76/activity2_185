228b14d4ef924a6e674fcb97058ee3e05c0cdfea
nur hikmah <nur.hikmah190504@gmail.com>
2023-03-20 18:47:27 +0700

    Add project files.

---
+++ latihan4_185/latihan4_185.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+float LuasPersegi(float p, float l) {
+	return p * l;
+}
+float LuasLingkaran(float r) {
+	return 3.14 * r * r;
+}
+float luasSegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
